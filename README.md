358c71b4684b410de780c33d766061eb07ab326c
zacanger <zac@zacanger.com>
2019-01-11 20:21:54 -0700

    I don't even know if this is a good idea

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # koa-env
 
+Koa middleware for getting and setting environment variables.
+
 --------
 
 ## Installation
 
+`npm i koa-env`
+
 ## Usage
+
+```javascript
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(koaEnv('/env-route'))
+
+app.listen(8080
+```
+
+```
+GET /env-route/foo // => process.env.foo
+GET /env-route // => process.env
+POST /env-route -d '{"foo": "bar"}' // => process.env.foo = bar
+```
 
 ## License
 
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-module.exports = {}
+const putenv = require('putenv')
+const parse = require('co-body')
+const getEnvVar = require('zeelib/lib/get-env-var')
+const getEnv = () => process.env
+
+module.exports = (prefix) =>
+  async (ctx, next) => {
+    ctx.type = 'application/json'
+    switch (ctx.request.method) {
+      case 'GET':
+        const envVar = ctx.request.path.replace(prefix, '').replace(/^\//, '')
+        if (envVar) {
+          ctx.body = JSON.stringify(getEnvVar(envVar))
+        } else {
+          ctx.body = JSON.stringify(getEnv())
+        }
+        return next()
+      case 'POST':
+        const kv = await parse.json(ctx)
+        Object.keys(kv).forEach((k) => {
+          putenv(k, kv[k])
+        })
+        ctx.body = null
+        return next()
+      default:
+        ctx.body = null
+        return next()
+    }
+  }
